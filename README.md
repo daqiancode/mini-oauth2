5003d622bf34cc0b3f6868e0ba10c9a9851c2bc4
daqian <daqiancode@gmail.com>
2025-06-17 11:16:01 +0400

    mobile login

--- README.md
+++ README.md
@@ -49,10 +49,10 @@ sudo docker run --name redis1 -d -p 6379:6379 redis
 
 # Build the image
 docker build -t daqiancode/mini-oauth2:0.0.1 .
-
+docker push daqiancode/mini-oauth2:0.0.1
 # Run the container
 docker run -p 3000:3000 --rm --env-file .env daqiancode/mini-oauth2:0.0.1
-docker push daqiancode/mini-oauth2:0.0.1
+
 
 # rebuild
 docker-compose down
--- app/routers/social_user.py
+++ app/routers/social_user.py
@@ -4,6 +4,7 @@ from app.config import env
 from app.services.oauth_client import GoogleOAuthClient, GithubOAuthClient, AppleOAuthClient, WechatOAuthClient, LinkedinOAuthClient
 import logging
 from pydantic import BaseModel
+from app.utils.jwts import create_access_token
 log = logging.getLogger(__name__)
 
 
@@ -11,10 +12,10 @@ router = APIRouter(tags=["Social Mobile"])
 
 
 class SocialIdTokenRequest(BaseModel):
-    id_token: str
+    access_token: str
     provider: str # google, apple, facebook, twitter, github, linkedin, wechat, qq, weibo, alipay
 
-@router.post("/social/user")
+@router.post("/social/user", description="Login with social access_token")
 async def social_id_token(request: Request , form: SocialIdTokenRequest = Depends(SocialIdTokenRequest)):
     if request.provider == "google":
         user = await GoogleOAuthClient(env.GOOGLE_CLIENT_ID, env.GOOGLE_CLIENT_SECRET).get_userinfo(request.id_token)
@@ -31,4 +32,5 @@ async def social_id_token(request: Request , form: SocialIdTokenRequest = Depend
     
     # save user to database
     user = await Users().save_or_update(user.get("name"), user.get("avatar"), user.get("email"), user.get("openid"), user.get("source"))
-    return user
+    jwt = create_access_token(env.JWT_PRIVATE_KEY, user.id, user.roles, env.JWT_EXPIRES_IN_HOURS * 60)
+    return {"access_token": jwt , "expires_in": env.JWT_EXPIRES_IN_HOURS * 60}
